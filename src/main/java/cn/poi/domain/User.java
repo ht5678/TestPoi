c72d5fd925e7bd1bc6dc4b53aca0c19b11690276
byht5678 <yzh5350@163.com>
2013-06-15 11:21:57 +0800

    user

---
+++ src/main/java/cn/poi/domain/User.java
@@ -0,0 +1,44 @@
+package cn.poi.domain;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author yuezhihua
+ *
+ */
+public class User implements Serializable{
+
+	public Integer id;
+	
+	private String uname;
+	
+	private String upass;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUpass() {
+		return upass;
+	}
+
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+
+	
+}
